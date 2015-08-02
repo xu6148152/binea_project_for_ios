8c913241e346efac3f3e53996df0e0aed97a142c
xubinggui <xubinggui@zepplabs.com>
2015-08-02 15:18:18 +0800

    add UITabBarViewController

---
+++ SportDemo/SportDemo/Shared/feature/Main/SportMainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SportMainViewController.swift
+//  SportDemo
+//
+//  Created by Binea Xu on 8/2/15.
+//  Copyright (c) 2015 Binea Xu. All rights reserved.
+//
+
+import UIKit
+
+class SportMainViewController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func showLoginView(){
+        if self.view.window != nil{
+            self.tabBar.hidden = true
+            dispatch_after(when: dispatch_time(DISPATCH_TIME_NOW, (Int64).1*NSEC_PER_SEC), queue: dispatch_get_main_queue(), { () -> Void in
+                self.showLoginView()
+            })
+        }else{
+            
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
