562c15b09fde08b3fa05c5b1259712831b60511d
xubinggui <xubinggui@zepplabs.com>
2015-10-13 12:04:27 +0800

    update

---
+++ CustomXib/CustomXib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomXib
+//
+//  Created by Binea Xu on 10/13/15.
+//  Copyright © 2015 Binea Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
